d75cbe5093c9296946be239086db7ad30b4d3ab3
Vitor Windberg <vitorwlima13@gmail.com>
2023-06-14 19:42:27 -0300

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="lp-container p-8">
+      <Logo />
+    </header>
+  );
+};
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo: React.FC = () => {
+  return (
+    <h1 className="text-2xl text-cyan-500 font-archivo font-bold tracking-wider">
+      Briefly
+    </h1>
+  );
+};
