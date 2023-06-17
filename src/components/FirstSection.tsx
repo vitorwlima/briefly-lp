aad7f7d2b3939427452c69dcb2d6b49fa4519b43
Vitor Lima <vitorwlima13@gmail.com>
2023-06-16 21:00:46 -0300

    feat: add supercharge custom underline

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -4,7 +4,10 @@ export const FirstSection: React.FC = () => {
   return (
     <section className="lp-container flex flex-col items-center justify-center py-10 text-white">
       <h2 className="max-w-xs text-center font-archivo text-2xl font-semibold tracking-wide sm:max-w-md sm:text-3xl lg:text-4xl">
-        Supercharge your social media growth today
+        <span className="relative before:absolute before:bottom-0 before:-z-10 before:h-2 before:w-full before:bg-sky-400 before:content-['']">
+          Supercharge
+        </span>{' '}
+        your social media growth today
       </h2>
 
       <p className="mt-10 max-w-xl text-center">
