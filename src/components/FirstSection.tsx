f85bdbaceaf16fb61a6985bce555ef4a16115781
Vitor Lima <vitorwlima13@gmail.com>
2023-06-16 21:40:37 -0300

    feat: add footer

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -3,9 +3,9 @@ import { Header } from './Header'
 
 export const FirstSection: React.FC = () => {
   return (
-    <div className="bg-[url('/stars-bg.svg')] bg-cover bg-no-repeat pb-20">
+    <div className="bg-sky-600 bg-[url('/stars-bg.svg')] bg-cover bg-no-repeat pb-20">
       <Header />
-      <section className="lp-container flex flex-col items-center justify-center  text-white">
+      <section className="lp-container mt-12 flex flex-col items-center  justify-center text-white">
         <h2 className="max-w-xs text-center font-archivo text-2xl font-semibold tracking-wide sm:max-w-md sm:text-3xl lg:text-4xl">
           <span className="relative before:absolute before:bottom-0 before:-z-10 before:h-2 before:w-full before:bg-sky-400 before:content-['']">
             Supercharge
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Logo } from './Logo'
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="bg-zinc-800">
+      <div className="lp-container p-8">
+        <Logo />
+      </div>
+    </footer>
+  )
+}
