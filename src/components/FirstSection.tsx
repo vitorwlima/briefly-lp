2e490448753e8ef0be7d76ed1227c9b38ac49842
Vitor Windberg <vitorwlima13@gmail.com>
2023-06-18 13:43:43 -0300

    fix: supercharge underline

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -7,7 +7,7 @@ export const FirstSection: React.FC = () => {
       <Header />
       <section className="lp-container mt-12 flex flex-col items-center  justify-center text-white">
         <h2 className="max-w-xs text-center font-archivo text-2xl font-semibold tracking-wide sm:max-w-md sm:text-3xl lg:text-4xl">
-          <span className="relative before:absolute before:bottom-0 before:-z-10 before:h-2 before:w-full before:bg-sky-400 before:content-['']">
+          <span className="relative z-20 before:absolute before:bottom-0 before:-z-10 before:h-2 before:w-full before:bg-sky-400 before:content-['']">
             Supercharge
           </span>{' '}
           your social media growth today
