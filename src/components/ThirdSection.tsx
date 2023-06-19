bd42000032a176afc15fadba5c101f74ca628d59
Vitor Windberg <vitorwlima13@gmail.com>
2023-06-19 20:17:12 -0300

    feat: improve third section padding

--- src/components/ThirdSection.tsx
+++ src/components/ThirdSection.tsx
@@ -3,7 +3,7 @@ import { CallToActionButtons } from './CallToActionButtons'
 export const ThirdSection: React.FC = () => {
   return (
     <div className="bg-zinc-700">
-      <section className="lp-container flex flex-col items-center py-20">
+      <section className="lp-container flex flex-col items-center py-12 xs:py-16 md:py-20">
         <h2 className="max-w-sm text-center text-2xl font-bold text-white sm:max-w-xl sm:text-3xl lg:max-w-2xl lg:text-4xl">
           Start supercharging your growth with short form content{' '}
           <span className="text-sky-400">today</span>
