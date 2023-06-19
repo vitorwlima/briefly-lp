388f7039ad599c98c0e75f1c077e3f6ee45b407c
Vitor Windberg <vitorwlima13@gmail.com>
2023-06-19 20:43:35 -0300

    fix: mobile buttons width

--- src/components/CallToActionButtons.tsx
+++ src/components/CallToActionButtons.tsx
@@ -2,7 +2,7 @@ import { Button } from './Button'
 
 export const CallToActionButtons: React.FC = () => {
   return (
-    <div className="mt-10 flex flex-col items-center justify-center gap-5 xs:flex-row">
+    <div className="mt-10 flex flex-col items-stretch justify-center gap-5 xs:flex-row xs:items-center">
       <Button variant="primary">Try 30 days trial</Button>
       <Button variant="secondary">Schedule a call</Button>
     </div>
